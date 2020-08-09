71460c044c68e219cb8f7d8b247a759336073a28
Staffan Selander <staffan.selander.li@gmail.com>
2020-08-09 09:47:52 +0200

    Reverting Assertion testing

--- test_strings.py
+++ test_strings.py
@@ -5,10 +5,10 @@ def test_interpolation():
     name = "Staffan"
 
     # Interpolate using string concatenation with the "+" operator.
-    assert "Hello " + name + "!" != "Hello Staffan!"
+    assert "Hello " + name + "!" == "Hello Staffan!"
 
     # Interpolate using %s.
-    assert "Hello %s!" % name != "Hello Staffan!"
+    assert "Hello %s!" % name == "Hello Staffan!"
 
     # Interpolate using Str.format().
     assert 'Hello {}!'.format(name) == "Hello Staffan!"
