48c412450f778a34b156fa6d549469b4c285130e
Staffan Selander <staffan.selander.li@gmail.com>
2020-08-09 09:32:37 +0200

    Strings

---
+++ test_strings.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def test_interpolation():
+    name = "Staffan"
+
+    # Interpolate using string concatenation with the "+" operator.
+    assert "Hello " + name + "!" != "Hello Staffan!"
+
+    # Interpolate using %s.
+    assert "Hello %s!" % name != "Hello Staffan!"
+
+    # Interpolate using Str.format().
+    assert 'Hello {}!'.format(name) == "Hello Staffan!"
+
+    # Interpolate using f-strings is possible after Python version 3.6 .
+    if sys.version_info.major >= 3 and sys.version_info.micro >= 6:
+        assert f"Hello {name}!" == "Hello Staffan!"
