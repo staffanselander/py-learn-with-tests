a70927bc4dafeaf0866dbdb6693a1c0261be5bc8
Staffan Selander <staffan.selander.li@gmail.com>
2020-08-15 20:35:32 +0200

    gs

--- test_strings.py
+++ test_strings.py
@@ -1,18 +1,29 @@
 import sys
 
 
-def test_interpolation():
+def test_interpolate_using_concatenation():
     name = "Staffan"
 
     # Interpolate using string concatenation with the "+" operator.
     assert "Hello " + name + "!" == "Hello Staffan!"
 
+
+def test_interpolate_using_percentage_s():
+    name = "Staffan"
+
     # Interpolate using %s.
     assert "Hello %s!" % name == "Hello Staffan!"
+
+
+def test_interpolate_using_str_format():
+    name = "Staffan"
 
     # Interpolate using Str.format().
     assert 'Hello {}!'.format(name) == "Hello Staffan!"
 
+
+def test_interpolate_using_f_strings():
+    name = "Staffan"
+
     # Interpolate using f-strings is possible after Python version 3.5.
-    if sys.version_info.major >= 3 and sys.version_info.micro >= 6:
-        assert f"Hello {name}!" == "Hello Staffan!"
+    assert f"Hello {name}!" == "Hello Staffan!"
